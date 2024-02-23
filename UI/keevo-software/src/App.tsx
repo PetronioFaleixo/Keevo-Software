1be601f47de379a46cde12dcb04bca49e4d0aa51
PetronioFaleixo <79844325+PetronioFaleixo@users.noreply.github.com>
2024-02-23 16:34:50 -0300

    instalação do eslint e do prettier

--- UI/keevo-software/src/App.tsx
+++ UI/keevo-software/src/App.tsx
@@ -1,38 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from './components/Button';
-import { Table } from './components/Table';
-import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from "react";
+import { Button } from "./components/Button";
+import { Table } from "./components/Table";
+import axios, { AxiosResponse } from "axios";
 
-interface ITarefa{
-descricao: string;
-id: number;
-nome: string;
-status: number;
+interface ITarefa {
+  descricao: string;
+  id: number;
+  nome: string;
+  status: number;
 }
 const App: React.FC = () => {
-const [tarefas, setTarefas] = useState<ITarefa[]>([]);
-const listar = async function fetchTarefas() {
-    const dados: AxiosResponse<ITarefa[]> =
-     await axios.get('https://localhost:7067/api/Tarefa/Listar');
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const listar = async function fetchTarefas() {
+    const dados: AxiosResponse<ITarefa[]> = await axios.get(
+      "https://localhost:7067/api/Tarefa/Listar"
+    );
     setTarefas(dados.data);
     console.log(dados.data);
-}
-    
+  };
+
   return (
     <>
-      <Button type={'secondary'} text="novo" iconCheck='fa-plus' eventOnClickButton={() => {
-        listar()
-      }}/>
+      <Button
+        type={"secondary"}
+        text="novo"
+        iconCheck="fa-plus"
+        eventOnClickButton={() => {
+          listar();
+        }}
+      />
 
-      <Table 
+      <Table
         columns={[
-          { title: 'Nome', field: 'nome' },
-          { title: 'Descrição', field: 'descricao' },
-          { title: 'Usuário', field: 'usuario' },
-          { title: 'Status', field: 'status' },
-          
+          { title: "Nome", field: "nome" },
+          { title: "Descrição", field: "descricao" },
+          { title: "Usuário", field: "usuario" },
+          { title: "Status", field: "status" },
         ]}
-        data={tarefas} />
+        data={tarefas}
+      />
     </>
   );
 };
